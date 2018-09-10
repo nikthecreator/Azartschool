b7447688a8a87623b3a0d52092153f268f82e076
nikthecreator <nikifor.shapran@gmail.com>
2018-09-10 19:03:38 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -23,3 +23,33 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+// -------------------------------------------------------------
+// Get the modal
+
+
+// Get the button that opens the modal
+
+
+// $(document).ready(function(){
+//     // Фикмированная шапка при скролле
+//     $("#header").removeClass("default");
+//     $(window).scroll(function(){
+//     	if ($(this).scrollTop() > 20) {
+//     		$("#header").addClass("default").fadeIn('fast');
+//     	} else {
+//     		$("#header").removeClass("default").fadeIn('fast');
+//     	};
+//     });
+
+//     Плавный скролл по якорям
+//     $('a[href^="#"]').click(function () { 
+//         elementClick = $(this).attr("href");
+//         destination = $(elementClick).offset().top;
+//         if($.browser.safari){
+//         $('body').animate( { scrollTop: destination }, 1000 );
+//         } else {
+//             $('html').animate( { scrollTop: destination }, 1000 );
+//         }
+//         return false;
+//    });
+// });
